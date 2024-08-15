7c54072e17d48f66744f449ac9daef4b4f1ed8e0
Bugra <bugrakaptan5@gmail.com>
2024-08-15 16:20:28 +0300

    snakegame

--- index.php
+++ index.php
@@ -69,6 +69,7 @@
           <li><a href="/projects/passwordgen/">Şifre Oluşturucu</a></li>
           <li><a href="/projects/sifre-kontrol/">Şifre Güçlülüğünü Kontrol Eder</a></li>
           <li><a href="/projects/store/">Sepet Sistemli Masaüstü Online Mağaza</a></li>
+          <li><a href="/projects/snakegame/">Yılan Oyunu || Tısssss</a></li>
         </ul>
       </div>
     </div>
