3ab181eff40f5dedc48fe4f821c01163ec9ea7c4
Bugra <bugrakaptan5@gmail.com>
2024-08-15 16:05:50 +0300

    proje

--- index.php
+++ index.php
@@ -1,1 +1,77 @@
-<h1>Yakında......</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bugra Akdemir - Projeler</title>
+  <link rel="icon" href="../assets/img/favicon.ico">
+
+  <style>
+    *{
+      text-decoration: none;
+      list-style-type: none;
+    }
+    body{
+      background-color: azure;
+    }
+
+    .cont{
+      background-color: #D4D2D5;
+      height: auto;
+      place-items: center;
+      display: grid;
+      text-align: center;
+
+    }
+    
+    li{
+      margin-bottom: 15px;
+      
+    }
+
+    .cont a{
+      color: #474056;
+      font-size: 17px;
+    }
+
+    nav{
+      background-color: #8FAD88;
+      text-align: center;
+      height: 30px;
+      place-items: center;
+      margin-bottom: 5px;
+    }
+
+    .navbar a{
+      color: white;
+    }
+
+  </style>
+
+</head>
+<body>
+  <nav>
+    <div class="navbar">
+      <ul>
+        <li><a href="/">Ana Sayfa</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="cont">
+    <div class="projects">
+      <div class="proje">
+        <ul>
+          <li><a href="/projects/duhanbilal/">Duhan Bilal Çoban || Web Şablon</a></li>
+          <li><a href="/projects/hava-durumu/">Hava Durumu Uygulaması</a></li>
+          <li><a href="/projects/hesap-mak/">Hesap Makinası</a></li>
+          <li><a href="/projects/notbook/">Şifreli Not Defteri</a></li>
+          <li><a href="/projects/random/">Rasgele Sayı Üreticisi</a></li>
+          <li><a href="/projects/passwordgen/">Şifre Oluşturucu</a></li>
+          <li><a href="/projects/sifre-kontrol/">Şifre Güçlülüğünü Kontrol Eder</a></li>
+          <li><a href="/projects/store/">Sepet Sistemli Masaüstü Online Mağaza</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
